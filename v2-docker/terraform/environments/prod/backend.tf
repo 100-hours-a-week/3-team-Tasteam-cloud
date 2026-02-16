f43fb1eaa22e2a618df2ec49df23e3f4fa91505a
Marcus Chae <chaebeomsoo@gmail.com>
2026-02-16 18:44:12 +0900

    v2 테라폼 환경별 구조 변경

--- v2-docker/terraform/environments/prod/backend.tf
+++ v2-docker/terraform/environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tasteam-v2-tfstate"
-    key          = "v2-docker/terraform.tfstate"
+    key          = "v2-docker/prod/terraform.tfstate"
     region       = "ap-northeast-2"
     profile      = "tasteam-v2"
     use_lockfile = true
---
+++ v2-docker/terraform/environments/prod/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "~> 1.14"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.32"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "tasteam-v2"
+}
