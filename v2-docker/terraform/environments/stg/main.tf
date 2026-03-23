03e7664fcfe1a7c5a55311fee30a6131cbdcd7ac
Seonjun Kim <kimsunjun1221@gmail.com>
2026-03-23 13:11:11 +0900

    feat(terraform): allow stg kafka connect write to prod analytics bucket

--- v2-docker/terraform/environments/stg/main.tf
+++ v2-docker/terraform/environments/stg/main.tf
@@ -804,6 +804,22 @@ resource "aws_iam_role_policy" "ec2_common_analytics_s3" {
         ]
         Resource = "${aws_s3_bucket.analytics.arn}/*"
       },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+        ]
+        Resource = "arn:aws:s3:::tasteam-prod-analytics"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+        ]
+        Resource = "arn:aws:s3:::tasteam-prod-analytics/*"
+      },
     ]
   })
 }
